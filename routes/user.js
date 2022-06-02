444a0a07937e93b1abe62930ba43952caf15bb55
SWAUGAT-BEURA <cse.190310300@silicon.ac.in>
2022-06-02 19:48:12 +0530

    changes in api

--- routes/user.js
+++ routes/user.js
@@ -6,9 +6,9 @@ const userController = require("../controllers/user.controller");
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.get("/getstudents", userController.getstudents);
-router
-  .route("/user/:id")
-  .get(userController.userInfo)
-  .put(userController.userInfoUpdate)
-  .delete(userController.userDelete);
+// router
+//   .route("/user/:id")
+//   .get(userController.userInfo)
+//   .put(userController.userInfoUpdate)
+//   .delete(userController.userDelete);
 module.exports = router;
